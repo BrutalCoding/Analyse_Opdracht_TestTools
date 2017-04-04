a3a9e0a4491e19a88194d1613d3bc67fb650e963
BrutalCoding <dhbreedeveld@gmail.com>
2017-04-05 00:33:58 +0200

    Added teardown for the test class

--- TestDemo.Test/HelloWorldTest.cs
+++ TestDemo.Test/HelloWorldTest.cs
@@ -13,32 +13,36 @@ namespace TestDemo.Test
     {
         HelloWorld HW;
         [SetUp]
-        public void Setup()
+        void Setup()
         {
             HW = new HelloWorld();
         }
 
         [TestCase]
-        public void CountHelloWorld()
+        void CountHelloWorld()
         {
             int response = HW.CountHelloWorld();
             Assert.AreEqual(11, response);
         }
 
         [TestCase]
-        public void ShowHelloWorld()
+        void ShowHelloWorld()
         {
             string response = HW.ShowHelloWorld();
             Assert.AreEqual("Hello World", response);
         }
 
         [TestCase]
-        public void LeapCalculator()
+        void LeapCalculator()
         {
             Boolean response = HW.LeapCalculator(1994);
             Assert.AreEqual(false, response);
         }
 
-
+        [TearDown]
+        void Uninitialize()
+        {
+            HW = null;
+        }
     }
 }
